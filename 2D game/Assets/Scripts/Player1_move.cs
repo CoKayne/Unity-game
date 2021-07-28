1c98f663f97f420b475bda741fc3e1927c698ad9
CoKayne <80445894+CoKayne@users.noreply.github.com>
2021-07-29 00:41:34 +0800

    Added player2 and comparing between two different moving system

---
+++ 2D game/Assets/Scripts/Player1_move.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player1_move : MonoBehaviour
+{
+    public float speed = 1f;
+    public float jumpforce = 1f;
+    public Rigidbody2D rb;
+
+    void start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    int moving_input(){
+        if(Input.GetKey("a")){
+            return -1;
+        }
+        if(Input.GetKey("d")){
+            return 1;
+        }
+        return 0;
+    }
+    void Update(){
+        var movement = Input.GetAxis("Horizontal");
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+
+        if(!Mathf.Approximately(0, movement)){
+            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        }
+
+        if(Input.GetKeyDown("w") && Mathf.Abs(rb.velocity.y) < 0.001f){
+            rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+        }
+    }       
+
+    void FixedUpdate() {
+       
+    }
+
+}
---
+++ 2D game/Assets/Scripts/Player2_move.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player2_move : MonoBehaviour
+{
+    public float speed = 1f;
+    public float jumpforce = 1f;
+    public Rigidbody2D rb;
+
+    void start(){
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    int moving_input(){
+        if(Input.GetKey("j")){
+            return -1;
+        }
+        if(Input.GetKey("l")){
+            return 1;
+        }
+        return 0;
+    }
+    void Update(){
+        var movement = moving_input();
+        transform.position += new Vector3(movement, 0, 0) * Time.deltaTime * speed;
+
+        if(!Mathf.Approximately(0, movement)){
+            transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
+        }
+
+        if(Input.GetKeyDown("i") && Mathf.Abs(rb.velocity.y) < 0.001f){
+            rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+        }
+    }       
+
+    
+
+}
