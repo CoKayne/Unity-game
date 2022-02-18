a8aa9aeee455a6af9b6615e1ea709d8d4dad4c97
CoKayne <80445894+CoKayne@users.noreply.github.com>
2022-02-19 00:38:35 +0800

    changed jump method

--- 2D game/Assets/Scripts/Player1_move.cs
+++ 2D game/Assets/Scripts/Player1_move.cs
@@ -8,17 +8,17 @@ public class Player1_move : MonoBehaviour
     public Transform groundCheck;
     public float checkRadius;
     public LayerMask whatIsGround;
-    private int extraJumps;
-    public int extraJumpsValue;
+    // private int extraJumps;
+    // public int extraJumpsValue;
     public float speed = 1f;
-
     public float jumpforce = 1f;
+    public float doubleJumpForce = 1f;
+    private bool doubleJump;
     public Rigidbody2D rb;
-
 
     void start(){
         rb = GetComponent<Rigidbody2D>();
-        extraJumps = extraJumpsValue;
+        // extraJumps = extraJumpsValue;
     }
 
    /* int moving_input(){               直接位移，座標移動
@@ -31,23 +31,42 @@ public class Player1_move : MonoBehaviour
         return 0;
     } */
     void Update(){
-        // if(Input.GetKeyDown("w")&& Mathf.Abs(rb.velocity.y) < 0.001f){
-        //     rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
-        //     SoundManagerScript.PlaySound("jump");
-        // }
-        if(isGrounded == true){
-            extraJumps = extraJumpsValue;
+
+/*        if(Input.GetKeyDown("w")&& Mathf.Abs(rb.velocity.y) < 0.001f){
+            rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
+            SoundManagerScript.PlaySound("jump");
+        }
+
+        if(isGrounded == true){ 
+                extraJumps = extraJumpsValue;
         }
 
         if(Input.GetKeyDown("w") && extraJumps > 0){
             rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
             SoundManagerScript.PlaySound("jump");
             extraJumps --;
-
         }else if(Input.GetKeyDown("w") && extraJumps == 0 && isGrounded == true){ 
             rb.AddForce(new Vector2(0, jumpforce), ForceMode2D.Impulse);
             SoundManagerScript.PlaySound("jump");
         }
+*/
+
+        if(isGrounded && !Input.GetKey("w")){
+            doubleJump = false;
+        }
+
+        if(Input.GetKeyDown("w")){
+            if(isGrounded || doubleJump){
+                rb.velocity = new Vector2(rb.velocity.x, doubleJump ? doubleJumpForce : jumpforce);
+                SoundManagerScript.PlaySound("jump");
+                doubleJump = !doubleJump;
+            }
+        }
+        
+        if(Input.GetKeyUp("w") && rb.velocity.y > 0f){
+            rb.velocity = new Vector2(rb.velocity.x, rb.velocity.y * 0.5f);
+        }
+
     }   
 
     void FixedUpdate() {
@@ -60,9 +79,7 @@ public class Player1_move : MonoBehaviour
         if(!Mathf.Approximately(0, movement)){
             transform.rotation = movement > 0 ? Quaternion.Euler(0, 180, 0) : Quaternion.identity;
         }
-
         
     }
-
 
 }
