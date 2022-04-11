abd53517f3a35b9f37272e72d68b763bee259f5f
CoKayne <80445894+CoKayne@users.noreply.github.com>
2022-04-11 11:04:20 +0800

    have to do rocket FX + character selection menu

--- 2D game/Assets/Scripts/Rocket_control.cs
+++ 2D game/Assets/Scripts/Rocket_control.cs
@@ -7,36 +7,38 @@ public class Rocket_control : MonoBehaviour
     public string[] rocketname;
     public GameObject[] rockets;
     public GameObject cloneroket;
-    public float rocketTimer=0f;
+    public float rocketTimer = 0f;
     public float rocketSpawntime = 0f;
     public string rocketToLaunch;
     public float decreaseTime = 0f;
     private bool spawnPlaceDecided = false;
-    public int randomnumber=0;
+    public int randomnumber = 0;
     public float launchx;
     public float launchy;
 
     void Start()
     {
-        
+
     }
     // Update is called once per frame
     void Update()
     {
         rocketTimer += Time.deltaTime;
-        if(rocketTimer > rocketSpawntime){  
-            randomnumber=Random.Range(0,rocketname.Length);
+        if (rocketTimer > rocketSpawntime)
+        {
+            randomnumber = Random.Range(0, rocketname.Length);
             rocketToLaunch = rocketname[randomnumber];
-            launchx=Random.Range (GameObject.Find(rocketToLaunch).GetComponent<Rocket_movement>().maxX,GameObject.Find(rocketToLaunch).GetComponent<Rocket_movement>().minX);
-            launchy=Random.Range (GameObject.Find(rocketToLaunch).GetComponent<Rocket_movement>().maxY,GameObject.Find(rocketToLaunch).GetComponent<Rocket_movement>().minY);
-            cloneroket=Instantiate(rockets[randomnumber],new Vector3 (launchx, launchy, 0),Quaternion.identity);
+            launchx = Random.Range(GameObject.Find(rocketToLaunch).GetComponent<Rocket_movement>().maxX, GameObject.Find(rocketToLaunch).GetComponent<Rocket_movement>().minX);
+            launchy = Random.Range(GameObject.Find(rocketToLaunch).GetComponent<Rocket_movement>().maxY, GameObject.Find(rocketToLaunch).GetComponent<Rocket_movement>().minY);
+            cloneroket = Instantiate(rockets[randomnumber], new Vector3(launchx, launchy, 0), Quaternion.identity);
             GameObject.Find(rocketToLaunch).GetComponentInChildren<WowMark_blink>().warningStart = true;
             rocketTimer = 0;
         }
-        Destroy(cloneroket,10);        
+        Destroy(cloneroket, 10);
     }
 
-    private void FixedUpdate() {
+    private void FixedUpdate()
+    {
 
     }
 }
